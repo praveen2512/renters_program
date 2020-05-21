bcbe7406f8cd60fbc23b0e3cb0e29a252b3f6a52
Praveen Kumar <praveenecemdu@gmail.com>
2020-05-21 14:26:35 +0530

    updated

--- client/src/components/pages/Quote.js
+++ client/src/components/pages/Quote.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
+//import { Redirect } from 'react-router-dom'
 import PolicyDetail from './PolicyDetail';
 import InsuredDetail from './InsuredDetail';
 import LocationDetail from './LocationDetail';
